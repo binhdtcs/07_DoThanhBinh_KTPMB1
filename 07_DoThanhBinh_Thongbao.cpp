445e357e982a30c3c05ad71839be66a52f3f934d
unknown <dtbinhcs@gmail.com>
2024-03-15 16:26:59 +0700

    sua doi test1

--- 07_DoThanhBinh_Thongbao.cpp
+++ 07_DoThanhBinh_Thongbao.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main{
 
 cout << "Hello" << endl;
-cout << "Hello" << endl;
-cout << "Hello" << endl;
+cout << "Hello cfcfcfcf" << endl;
+cout << "Hello 4444444444" << endl;
 system("pause");
 return 0;
 }
